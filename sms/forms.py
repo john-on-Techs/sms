c4e58db7bbc97c1a82c6df39ccaa5978344a62da
john-on-Techs <gavanayeye@gmail.com>
2019-02-12 21:43:10 +0300

    deleted forms.py

--- sms/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Station
-
-class StationForm(forms.ModelForm):
-    class Meta:
-        model = Station
-        fields = ['name']
-    def get_queryset(self):
-        return Station.objects.all()
